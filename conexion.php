3baa4ee6713c521fecc26df7271738394a5f2698
marquexmar <lunagonzalesmarco@gmail.com>
2020-03-30 16:33:05 -0400

    actualizacion menu

--- conexion.php
+++ conexion.php
@@ -1,12 +1,12 @@
 <?php 
 class Conexion extends PDO {    
  
- /*private $tipo_de_base = 'mysql';
+ private $tipo_de_base = 'mysql';
  private $host = 'localhost';
  private $nombre_de_base = 'ibnfinanciero';
  private $usuario = 'root';
  private $contrasena = '';
- private $port = '3306';
+ private $port = '3307';
 
 
  // private $tipo_de_base = 'mysql';
@@ -16,14 +16,15 @@ class Conexion extends PDO {
  // private $contrasena = '';
  // private $port = '3306';
 
-*/
- private $tipo_de_base = 'mysql';
+
+
+/* private $tipo_de_base = 'mysql';
  private $host = 'localhost';
  private $nombre_de_base = 'ibno_conta2';
  private $usuario = 'root';
  private $contrasena = '';
  private $port = '3306';
-
+*/
 
 
   // private $tipo_de_base = 'mysql';
