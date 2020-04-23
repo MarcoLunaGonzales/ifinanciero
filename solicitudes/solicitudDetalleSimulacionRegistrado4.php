1553d6823b06d3723b73e2ece9634510c9a266d4
davidhuarina25@gmail.com <73082343tigre3>
2020-04-23 14:36:34 -0400

    actualizacion solicitud de recursos v1

---
+++ solicitudes/solicitudDetalleSimulacionRegistrado4.php
@@ -0,0 +1,52 @@
+<?php
+						//$detalle=obtenerDetalleSolicitudSimulacion($codSimulacionServX);
+                        $codigoPlantillaXX=obtenerPlantillaCodigoSimulacionServicio($codSimulacionServX);
+                        $detalle=obtenerDetalleSolicitudSimulacionCuentaPlantillaServicio($codSimulacionServX,$codigoPlantillaXX);
+                        $ibnorca=1;
+                        $unidadSol=$codUnidadX;
+                        $areaSol=$codAreaX;
+						$idFila=1;
+						$cuentasCodigos=[];$conta=0;$auxAnio=0;$detalleAux="";$contAux=0;
+					?><div id="detalles_solicitud"><?php
+							 
+						
+                         
+                       $solicitudDetalle=obtenerSolicitudRecursosDetalle($codigo);
+                       while ($rowDetalles = $solicitudDetalle->fetch(PDO::FETCH_ASSOC)) {
+                          $tituloImporte="Importe";
+                          $codCuentaX=$rowDetalles['cod_plancuenta'];
+                          $codigoDetX=$rowDetalles["codigo"];
+                          $encontrar=0;
+                          for ($i=0; $i < count($cuentasCodigos); $i++) { 
+                            if($codigoDetX==$cuentasCodigos[$i]){
+                              $encontrar++;
+                            }	  
+						  }
+						 if($encontrar==0){
+						 	?><script>var nfac=[];itemFacturas.push(nfac);</script><?php
+                               $codDetalleX=$rowDetalles["codigo"];	
+                               $detalleX=$rowDetalles["detalle"];
+                               $importeX=$rowDetalles["importe_presupuesto"];
+							   $importeSolX=$rowDetalles["importe"];
+							   $proveedorX=$rowDetalles["cod_proveedor"];
+							   $retencionX=$rowDetalles["cod_confretencion"];
+							   if($retencionX!=0){
+							   	$tituloImporte="Importe - ".nameRetencion($retencionX);
+							   	if(strlen($tituloImporte)>13){
+                                   $tituloImporte=substr($tituloImporte,0,13)."...";
+                                 }
+							   }
+							   $numeroCuentaX=trim($rowDetalles['numero']);
+							   $nombreCuentaX=trim($rowDetalles['nombre']);
+							   $nombrePartidaX="PARTIDA PRESUPUESTARIA";
+							   $nombrePartidaDetalleX="Cuenta";
+							   include "addFila.php";
+                         			 
+						       $idFila=$idFila+1;
+						 } 
+                       }
+						
+
+?>
+                   </div>
+	<script>$("#cantidad_filas").val(<?=($idFila-1)?>)</script>					
