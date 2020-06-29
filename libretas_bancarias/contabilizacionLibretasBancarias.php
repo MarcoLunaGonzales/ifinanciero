f57dae6c706ffda252300b9b09ac149a095677d3
davidhuarina25@gmail.com <73082343tigre3>
2020-06-28 22:27:33 -0400

    actuyalizacion deposito pagos v2

--- libretas_bancarias/contabilizacionLibretasBancarias.php
+++ libretas_bancarias/contabilizacionLibretasBancarias.php
@@ -15,11 +15,19 @@ $dbh = new Conexion();
 set_time_limit(300);
 $fechaActual=date("Y-m-d");
 
-$fecha=$_POST['fecha_desde'];
+$gestion=nameGestion($_POST['gestion']);
+$mes=$_POST['cod_mes_x'];
+if($mes<10){
+  $mes="0".$mes;
+}
+
+$dia=date("d",(mktime(0,0,0,$mes+1,1,$gestion)-1));
+
+$fecha=$gestion."-".$mes."-01";
 $fechaTitulo= explode("-",$fecha);
 $fechaFormateada=$fechaTitulo[2].'/'.$fechaTitulo[1].'/'.$fechaTitulo[0];
 
-$fechaHasta=$_POST['fecha_hasta'];
+$fechaHasta=$gestion."-".$mes."-".$dia;
 $fechaTituloHasta= explode("-",$fechaHasta);
 $fechaFormateadaHasta=$fechaTituloHasta[2].'/'.$fechaTituloHasta[1].'/'.$fechaTituloHasta[0];
 
