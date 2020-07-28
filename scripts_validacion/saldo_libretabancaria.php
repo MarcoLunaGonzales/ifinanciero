6ccf0322f2880e06795906248291e12f62760fef
marquexmar <lunagonzalesmarco@gmail.com>
2020-07-28 01:25:29 -0400

    revisando libretas

---
+++ scripts_validacion/saldo_libretabancaria.php
@@ -0,0 +1,12 @@
+<?php
+require_once '../layouts/bodylogin2.php';
+require_once '../conexion.php';
+require_once '../styles.php';
+require_once '../functions.php';
+require_once '../functionsGeneral.php';
+
+$dbh = new Conexion();
+
+
+
+?>
