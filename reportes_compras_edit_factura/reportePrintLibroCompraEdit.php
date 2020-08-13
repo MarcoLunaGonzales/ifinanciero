3af2db950bfb21a8681c653d4dbbf5262a88b5ca
davidhuarina25@gmail.com <73082343tigre3>
2020-08-13 11:02:35 -0400

    actualizar copmrobantes SAVE PLANTILLA 4

--- reportes_compras_edit_factura/reportePrintLibroCompraEdit.php
+++ reportes_compras_edit_factura/reportePrintLibroCompraEdit.php
@@ -80,7 +80,7 @@ $razon_social=$result['razon_social'];
                   <div class="card-icon bg-blanco">
                     <img class="" width="60" height="60" src="../assets/img/logo_ibnorca_origen.png">
                   </div>                  
-                  <h3 class="card-title text-center" ><b>Libro de Compras IVA- Proyecto</b>
+                  <h3 class="card-title text-center" ><b>Libro de Compras - Edición</b>
                     <span><br><h6>
                     Del Período: <?=$nombre_mes;?>/<?=$nombre_gestion;?><br>
                     Expresado En Bolivianos</h6></span></h3>                  
