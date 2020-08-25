8f8e61a324c02eab17dc11778c24fdcb0b224c59
Ismael <sullcamani@gmail.com>
2020-08-25 15:34:35 -0500

    commit obs 25_08_2020

--- solicitud_facturacion_manual/save_solicitud_facturacion.php
+++ solicitud_facturacion_manual/save_solicitud_facturacion.php
@@ -45,135 +45,154 @@ try {
     }
     if (isset($_POST["dias_credito"]))$dias_credito = $_POST["dias_credito"];
     else $dias_credito = '';
-    if ($cod_facturacion == 0){//insertamos    
-        $nro_correlativo=obtenerCorrelativoSolicitud();//correlativo
-        $stmt = $dbh->prepare("INSERT INTO solicitudes_facturacion(cod_simulacion_servicio,cod_unidadorganizacional,cod_area,fecha_registro,fecha_solicitudfactura,cod_tipoobjeto,cod_tipopago,cod_cliente,cod_personal,razon_social,nit,observaciones,observaciones_2,nro_correlativo,persona_contacto,cod_estadosolicitudfacturacion,codigo_alterno,tipo_solicitud,dias_credito) 
-        values ('$cod_simulacion','$cod_unidadorganizacional','$cod_area','$fecha_registro','$fecha_solicitudfactura','$cod_tipoobjeto','$cod_tipopago','$cod_cliente','$cod_personal','$razon_social','$nit','$observaciones','$observaciones_2','$nro_correlativo','$persona_contacto',1,'$codigo_alterno',4,'$dias_credito')");//4 tipo solicitud manual
-        $flagSuccess=$stmt->execute();
-        // $flagSuccess=true;
-        if($flagSuccess){
-            //sacamos el codigo insertado
-           $stmt = $dbh->prepare("SELECT codigo from solicitudes_facturacion where cod_simulacion_servicio=$cod_simulacion ORDER BY codigo desc LIMIT 1");
-           $stmt->execute();           
-           while ($rowPre = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                  $cod_facturacion=$rowPre['codigo'];
-            }           
-            //los agregados con  ajax
-            $cantidad_filas = $_POST["cantidad_filas"];
-            for ($i=1;$i<=$cantidad_filas;$i++){                
-                $servicioInsert_ajax=$_POST["modal_editservicio".$i];
-                $CantidadInsert_ajax=$_POST["cantidad_servicios".$i];
-                // $importeInsert_ajax=$_POST["modal_importe_add".$i];
-                // $importeInsert_ajax=$_POST["modal_montoserv".$i];
-                $DescricpionInsert_ajax=$_POST["descripcion".$i];  
-                $importeInsert_ajax=$_POST["modal_importe_add".$i]/$CantidadInsert_ajax;
-                $descuento_por_Insert_ajax=$_POST["descuento_por_add".$i];
-                $descuento_bob_Insert_ajax=$_POST["descuento_bob_add".$i]; 
-                $sql="INSERT INTO solicitudes_facturaciondetalle(cod_solicitudfacturacion,cod_claservicio,cantidad,precio,descripcion_alterna,descuento_por,descuento_bob,tipo_item) 
-                values ('$cod_facturacion','$servicioInsert_ajax','$CantidadInsert_ajax','$importeInsert_ajax','$DescricpionInsert_ajax','$descuento_por_Insert_ajax','$descuento_bob_Insert_ajax',2)";
-                $stmt = $dbh->prepare($sql);                
-                $flagSuccess=$stmt->execute();                
+    if(!isset($_POST["cod_uo"]) && !isset($_POST["cod_area"])){
+        $cod_area=0;
+        $cod_unidadorganizacional=0;
+    }
+    if($cod_area!=null && $cod_area!=0 && $cod_area!="" && $cod_unidadorganizacional!=null && $cod_unidadorganizacional!=0 && $cod_unidadorganizacional!=""){
+        if ($cod_facturacion == 0){//insertamos    
+            $nro_correlativo=obtenerCorrelativoSolicitud();//correlativo
+            $stmt = $dbh->prepare("INSERT INTO solicitudes_facturacion(cod_simulacion_servicio,cod_unidadorganizacional,cod_area,fecha_registro,fecha_solicitudfactura,cod_tipoobjeto,cod_tipopago,cod_cliente,cod_personal,razon_social,nit,observaciones,observaciones_2,nro_correlativo,persona_contacto,cod_estadosolicitudfacturacion,codigo_alterno,tipo_solicitud,dias_credito) 
+            values ('$cod_simulacion','$cod_unidadorganizacional','$cod_area','$fecha_registro','$fecha_solicitudfactura','$cod_tipoobjeto','$cod_tipopago','$cod_cliente','$cod_personal','$razon_social','$nit','$observaciones','$observaciones_2','$nro_correlativo','$persona_contacto',1,'$codigo_alterno',4,'$dias_credito')");//4 tipo solicitud manual
+            $flagSuccess=$stmt->execute();
+            // $flagSuccess=true;
+            if($flagSuccess){
+                //sacamos el codigo insertado
+               $stmt = $dbh->prepare("SELECT codigo from solicitudes_facturacion where cod_simulacion_servicio=$cod_simulacion ORDER BY codigo desc LIMIT 1");
+               $stmt->execute();           
+               while ($rowPre = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                      $cod_facturacion=$rowPre['codigo'];
+                }           
+                //los agregados con  ajax
+                $cantidad_filas = $_POST["cantidad_filas"];
+                for ($i=1;$i<=$cantidad_filas;$i++){                
+                    $servicioInsert_ajax=$_POST["modal_editservicio".$i];
+                    $CantidadInsert_ajax=$_POST["cantidad_servicios".$i];
+                    // $importeInsert_ajax=$_POST["modal_importe_add".$i];
+                    // $importeInsert_ajax=$_POST["modal_montoserv".$i];
+                    $DescricpionInsert_ajax=$_POST["descripcion".$i];  
+                    $importeInsert_ajax=$_POST["modal_importe_add".$i]/$CantidadInsert_ajax;
+                    $importeInsert_ajax=round($importeInsert_ajax,2);
+                    $descuento_por_Insert_ajax=$_POST["descuento_por_add".$i];
+                    $descuento_bob_Insert_ajax=$_POST["descuento_bob_add".$i]; 
+                    $sql="INSERT INTO solicitudes_facturaciondetalle(cod_solicitudfacturacion,cod_claservicio,cantidad,precio,descripcion_alterna,descuento_por,descuento_bob,tipo_item) 
+                    values ('$cod_facturacion','$servicioInsert_ajax','$CantidadInsert_ajax','$importeInsert_ajax','$DescricpionInsert_ajax','$descuento_por_Insert_ajax','$descuento_bob_Insert_ajax',2)";
+                    $stmt = $dbh->prepare($sql);                
+                    $flagSuccess=$stmt->execute();                
+                }
+                //======================================
+                //para distribucion de tipo de pagos y areas
+                $tipo_solicitud=4;
+                require_once '../simulaciones_servicios/save_distribucion_montos_solfac.php';
+                //borramos los archivos
+                $sqlDel="DELETE FROM archivos_adjuntos_solicitud_facturacion where cod_solicitud_facturacion=$cod_facturacion";
+                $stmtDel = $dbh->prepare($sqlDel);
+                $stmtDel->execute();
+                require_once '../simulaciones_servicios/insertar_archivosadjuntos.php';
             }
-            //======================================
-            //para distribucion de tipo de pagos y areas
-            $tipo_solicitud=4;
-            require_once '../simulaciones_servicios/save_distribucion_montos_solfac.php';
-            //borramos los archivos
-            $sqlDel="DELETE FROM archivos_adjuntos_solicitud_facturacion where cod_solicitud_facturacion=$cod_facturacion";
-            $stmtDel = $dbh->prepare($sqlDel);
-            $stmtDel->execute();
-            require_once '../simulaciones_servicios/insertar_archivosadjuntos.php';
-        }
-        //enviar propuestas para la actualizacion de ibnorca
-        date_default_timezone_set('America/La_Paz');
-        $fechaHoraActual=date("Y-m-d H:i:s");
-        $idTipoObjeto=2709;
-        $idObjeto=2726; //regristado
-        $obs="Registro de Solicitud Facturación";
-        if(isset($_POST['usuario_ibnored_u'])){
-            $u=$_POST['usuario_ibnored_u'];
-            actualizarEstadosObjetosIbnorca($idTipoObjeto,$idObjeto,$u,$cod_facturacion,$fechaHoraActual,$obs);
-        }else{
-            actualizarEstadosObjetosIbnorca($idTipoObjeto,$idObjeto,$globalUser,$cod_facturacion,$fechaHoraActual,$obs);
-        }
-        if(isset($_POST['usuario_ibnored'])){
-            $q=$_POST['usuario_ibnored'];
-            $v=$_POST['usuario_ibnored_v'];
-            $s=$_POST['usuario_ibnored_s'];
-            $u=$_POST['usuario_ibnored_u'];
-            showAlertSuccessError($flagSuccess,"../".$urlSolicitudfactura_2."&q=".$q."&v=".$v."&s=".$s."&u=".$u);
-        }else{
-            showAlertSuccessError($flagSuccess,"../".$urlSolicitudfactura_2);  
-        }
-        
+            //enviar propuestas para la actualizacion de ibnorca
+            date_default_timezone_set('America/La_Paz');
+            $fechaHoraActual=date("Y-m-d H:i:s");
+            $idTipoObjeto=2709;
+            $idObjeto=2726; //regristado
+            $obs="Registro de Solicitud Facturación";
+            if(isset($_POST['usuario_ibnored_u'])){
+                $u=$_POST['usuario_ibnored_u'];
+                actualizarEstadosObjetosIbnorca($idTipoObjeto,$idObjeto,$u,$cod_facturacion,$fechaHoraActual,$obs);
+            }else{
+                actualizarEstadosObjetosIbnorca($idTipoObjeto,$idObjeto,$globalUser,$cod_facturacion,$fechaHoraActual,$obs);
+            }
+            if(isset($_POST['usuario_ibnored'])){
+                $q=$_POST['usuario_ibnored'];
+                $v=$_POST['usuario_ibnored_v'];
+                $s=$_POST['usuario_ibnored_s'];
+                $u=$_POST['usuario_ibnored_u'];
+                showAlertSuccessError($flagSuccess,"../".$urlSolicitudfactura_2."&q=".$q."&v=".$v."&s=".$s."&u=".$u);
+            }else{
+                showAlertSuccessError($flagSuccess,"../".$urlSolicitudfactura_2);  
+            }
+            
 
-        //$stmt->debugDumpParams();
-    }else{//update
-        //actualizamos los campos estaticos
-        $stmt = $dbh->prepare("UPDATE solicitudes_facturacion set cod_unidadorganizacional='$cod_unidadorganizacional',cod_area='$cod_area',fecha_registro='$fecha_registro',fecha_solicitudfactura='$fecha_solicitudfactura',cod_tipoobjeto='$cod_tipoobjeto',cod_tipopago='$cod_tipopago',cod_cliente='$cod_cliente',cod_personal='$cod_personal',razon_social='$razon_social',nit='$nit',observaciones='$observaciones',observaciones_2='$observaciones_2',persona_contacto='$persona_contacto',dias_credito = '$dias_credito'
-        where codigo = $cod_facturacion");      
-        $flagSuccess=$stmt->execute();
-        if($flagSuccess){
-            //borramos los datos y insertados en detalle
-            $stmtDelete = $dbh->prepare("DELETE from solicitudes_facturaciondetalle where cod_solicitudfacturacion= $cod_facturacion");      
-            $stmtDelete->execute();
-            for ($i=1;$i<=$modal_numeroservicio-1;$i++){
-                $servicioInsert="";
-                $CantidadInsert="";
-                $importeInsert="";
-                $DescricpionInsert="";                
-                if(isset($_POST["servicio".$i])){
-                    $servicioInsert=$_POST["servicio_a".$i];
-                    $CantidadInsert=$_POST["cantidad_a".$i];
-                    // $importeInsert=$_POST["modal_importe".$i];
-                    $importeInsert=$_POST["modal_importe".$i]/$CantidadInsert;
-                    
-                    $DescricpionInsert=$_POST["descripcion_alterna".$i];
-                    $descuento_por_Insert=$_POST["descuento_por".$i];
-                    $descuento_bob_Insert=$_POST["descuento_bob".$i];
+            //$stmt->debugDumpParams();
+        }else{//update
+            //actualizamos los campos estaticos
+            $stmt = $dbh->prepare("UPDATE solicitudes_facturacion set cod_unidadorganizacional='$cod_unidadorganizacional',cod_area='$cod_area',fecha_registro='$fecha_registro',fecha_solicitudfactura='$fecha_solicitudfactura',cod_tipoobjeto='$cod_tipoobjeto',cod_tipopago='$cod_tipopago',cod_cliente='$cod_cliente',cod_personal='$cod_personal',razon_social='$razon_social',nit='$nit',observaciones='$observaciones',observaciones_2='$observaciones_2',persona_contacto='$persona_contacto',dias_credito = '$dias_credito'
+            where codigo = $cod_facturacion");      
+            $flagSuccess=$stmt->execute();
+            if($flagSuccess){
+                //borramos los datos y insertados en detalle
+                $stmtDelete = $dbh->prepare("DELETE from solicitudes_facturaciondetalle where cod_solicitudfacturacion= $cod_facturacion");      
+                $stmtDelete->execute();
+                for ($i=1;$i<=$modal_numeroservicio-1;$i++){
+                    $servicioInsert="";
+                    $CantidadInsert="";
+                    $importeInsert="";
+                    $DescricpionInsert="";                
+                    if(isset($_POST["servicio".$i])){
+                        $servicioInsert=$_POST["servicio_a".$i];
+                        $CantidadInsert=$_POST["cantidad_a".$i];
+                        // $importeInsert=$_POST["modal_importe".$i];
+                        $importeInsert=$_POST["modal_importe".$i]/$CantidadInsert;
+                        $importeInsert=round($importeInsert,2);
+                        
+                        $DescricpionInsert=$_POST["descripcion_alterna".$i];
+                        $descuento_por_Insert=$_POST["descuento_por".$i];
+                        $descuento_bob_Insert=$_POST["descuento_bob".$i];
+                    }
+                    if($servicioInsert!=0 || $servicioInsert!=""){
+                        $stmt = $dbh->prepare("INSERT INTO solicitudes_facturaciondetalle(cod_solicitudfacturacion,cod_claservicio,cantidad,precio,descripcion_alterna,descuento_por,descuento_bob,tipo_item) 
+                        values ('$cod_facturacion','$servicioInsert','$CantidadInsert','$importeInsert','$DescricpionInsert','$descuento_por_Insert','$descuento_bob_Insert',2)");//porque fue introducido desde servicios adicionales
+                        $flagSuccess=$stmt->execute();
+                    }
                 }
-                if($servicioInsert!=0 || $servicioInsert!=""){
-                    $stmt = $dbh->prepare("INSERT INTO solicitudes_facturaciondetalle(cod_solicitudfacturacion,cod_claservicio,cantidad,precio,descripcion_alterna,descuento_por,descuento_bob,tipo_item) 
-                    values ('$cod_facturacion','$servicioInsert','$CantidadInsert','$importeInsert','$DescricpionInsert','$descuento_por_Insert','$descuento_bob_Insert',2)");//porque fue introducido desde servicios adicionales
-                    $flagSuccess=$stmt->execute();
+                 //los agregados con  ajax
+                $cantidad_filas = $_POST["cantidad_filas"];
+                for ($i=1;$i<=$cantidad_filas;$i++){                
+                    $servicioInsert_ajax=$_POST["modal_editservicio".$i];
+                    $CantidadInsert_ajax=$_POST["cantidad_servicios".$i];
+                    // $importeInsert_ajax=$_POST["modal_montoserv".$i];
+                    $DescricpionInsert_ajax=$_POST["descripcion".$i];  
+                    $importeInsert_ajax=$_POST["modal_importe_add".$i]/$CantidadInsert_ajax;
+                    $importeInsert_ajax=round($importeInsert_ajax,2);
+                    // $importeInsert=$_POST["monto_precio".$i];
+                    $descuento_por_Insert_ajax=$_POST["descuento_por_add".$i];
+                    $descuento_bob_Insert_ajax=$_POST["descuento_bob_add".$i]; 
+                    $sql="INSERT INTO solicitudes_facturaciondetalle(cod_solicitudfacturacion,cod_claservicio,cantidad,precio,descripcion_alterna,descuento_por,descuento_bob,tipo_item) 
+                    values ('$cod_facturacion','$servicioInsert_ajax','$CantidadInsert_ajax','$importeInsert_ajax','$DescricpionInsert_ajax','$descuento_por_Insert_ajax','$descuento_bob_Insert_ajax',2)";
+                    $stmt = $dbh->prepare($sql);                
+                    $flagSuccess=$stmt->execute();                
                 }
+                //======================================
+                //para distribucion de tipo de pagos y areas
+                $tipo_solicitud=4;
+                require_once '../simulaciones_servicios/save_distribucion_montos_solfac.php';
+                //borramos los archivos
+                // $sqlDel="DELETE FROM archivos_adjuntos_solicitud_facturacion where cod_solicitud_facturacion=$cod_facturacion";
+                // $stmtDel = $dbh->prepare($sqlDel);
+                // $stmtDel->execute();
+                //subir archivos al servidor
+                //Como el elemento es un arreglos utilizamos foreach para extraer todos los valores
+                require_once '../simulaciones_servicios/insertar_archivosadjuntos.php';
             }
-             //los agregados con  ajax
-            $cantidad_filas = $_POST["cantidad_filas"];
-            for ($i=1;$i<=$cantidad_filas;$i++){                
-                $servicioInsert_ajax=$_POST["modal_editservicio".$i];
-                $CantidadInsert_ajax=$_POST["cantidad_servicios".$i];
-                // $importeInsert_ajax=$_POST["modal_montoserv".$i];
-                $DescricpionInsert_ajax=$_POST["descripcion".$i];  
-                $importeInsert_ajax=$_POST["modal_importe_add".$i]/$CantidadInsert_ajax;
-                // $importeInsert=$_POST["monto_precio".$i];
-                $descuento_por_Insert_ajax=$_POST["descuento_por_add".$i];
-                $descuento_bob_Insert_ajax=$_POST["descuento_bob_add".$i]; 
-                $sql="INSERT INTO solicitudes_facturaciondetalle(cod_solicitudfacturacion,cod_claservicio,cantidad,precio,descripcion_alterna,descuento_por,descuento_bob,tipo_item) 
-                values ('$cod_facturacion','$servicioInsert_ajax','$CantidadInsert_ajax','$importeInsert_ajax','$DescricpionInsert_ajax','$descuento_por_Insert_ajax','$descuento_bob_Insert_ajax',2)";
-                $stmt = $dbh->prepare($sql);                
-                $flagSuccess=$stmt->execute();                
+            if(isset($_POST['usuario_ibnored'])){
+                $q=$_POST['usuario_ibnored'];
+                $v=$_POST['usuario_ibnored_v'];
+                $s=$_POST['usuario_ibnored_s'];
+                $u=$_POST['usuario_ibnored_u'];
+                showAlertSuccessError($flagSuccess,"../".$urlSolicitudfactura_2."&q=".$q."&v=".$v."&s=".$s."&u=".$u);
+            }else{
+                showAlertSuccessError($flagSuccess,"../".$urlSolicitudfactura_2);  
             }
-            //======================================
-            //para distribucion de tipo de pagos y areas
-            $tipo_solicitud=4;
-            require_once '../simulaciones_servicios/save_distribucion_montos_solfac.php';
-            //borramos los archivos
-            // $sqlDel="DELETE FROM archivos_adjuntos_solicitud_facturacion where cod_solicitud_facturacion=$cod_facturacion";
-            // $stmtDel = $dbh->prepare($sqlDel);
-            // $stmtDel->execute();
-            //subir archivos al servidor
-            //Como el elemento es un arreglos utilizamos foreach para extraer todos los valores
-            require_once '../simulaciones_servicios/insertar_archivosadjuntos.php';
         }
+    }else{
         if(isset($_POST['usuario_ibnored'])){
             $q=$_POST['usuario_ibnored'];
             $v=$_POST['usuario_ibnored_v'];
             $s=$_POST['usuario_ibnored_s'];
             $u=$_POST['usuario_ibnored_u'];
-            showAlertSuccessError($flagSuccess,"../".$urlSolicitudfactura_2."&q=".$q."&v=".$v."&s=".$s."&u=".$u);
+            showAlertSuccessError(false,"../".$urlSolicitudfactura_2."&q=".$q."&v=".$v."&s=".$s."&u=".$u);
         }else{
-            showAlertSuccessError($flagSuccess,"../".$urlSolicitudfactura_2);  
+            showAlertSuccessError(false,"../".$urlSolicitudfactura_2);  
         }
     }
 } catch(PDOException $ex){
