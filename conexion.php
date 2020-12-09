24339acfb5ea3b1265dcba19fe2f0fdfd1b03cc6
marquexmar <lunagonzalesmarco@gmail.com>
2020-12-09 09:52:12 -0400

    conexion

--- conexion.php
+++ conexion.php
@@ -2,10 +2,10 @@
 class Conexion extends PDO {    
 
   private $tipo_de_base = 'mysql';
-  private $host = 'localhost';  
-  private $nombre_de_base = 'ibnfinanciero1000';
-  private $usuario = 'root';
-  private $contrasena = '';
+  private $host = '192.168.30.35';  
+  private $nombre_de_base = 'bdifinanciero';
+  private $usuario = 'ingresobd';
+  private $contrasena = 'ingresoibno';
   private $port = '3306';
 
 public function __construct() {
