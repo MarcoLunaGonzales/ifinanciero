90de3498148e2d5c1c86a6d023e83dd14e7fce8a
isullcamani <sullcamani@gmail.com>
2020-12-17 15:18:44 -0400

    push ismael diciembre 2020

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@ class Conexion extends PDO {
 
   private $tipo_de_base = 'mysql';
   private $host = 'localhost';  
-  private $nombre_de_base = 'ibnfinanciero8000';
+  private $nombre_de_base = 'ibnfinanciero2000';
 
   private $usuario = 'root';
   private $contrasena = '';
