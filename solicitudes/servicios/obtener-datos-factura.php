329308b96df4da45c831076fa88a60fd824b1a41
David Huarina <davidhuarina25@gmail.com>
2020-12-18 10:10:17 -0400

    actualizacion servicio obtenecion fecha y validacion facturas

---
+++ solicitudes/servicios/obtener-datos-factura.php
@@ -0,0 +1,28 @@
+<?php 
+//$direccion='http://ibnored.ibnorca.org/ifinanciero/wsifin/';
+$direccion='http://127.0.0.1/ifinanciero/wsifin/';
+$sIde = "facifin";
+$sKey = "rrf656nb2396k6g6x44434h56jzx5g6";
+
+//$parametros=array("sIdentificador"=>$sIde, "sKey"=>$sKey, "accion"=>"VerificacionPagosCurso");
+$parametros=array("sIdentificador"=>$sIde, "sKey"=>$sKey, "accion"=>"VerificacionPagosCurso","IdCurso"=>2572,"Ci"=>"11498699");
+		
+		$parametros=json_encode($parametros);
+		// abrimos la sesión cURL
+		$ch = curl_init();
+		// definimos la URL a la que hacemos la petición
+		curl_setopt($ch, CURLOPT_URL,$direccion."ws_obtener_factura.php"); 
+		// indicamos el tipo de petición: POST
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		// definimos cada uno de los parámetros
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $parametros);
+		// recibimos la respuesta y la guardamos en una variable
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$remote_server_output = curl_exec ($ch);
+		// cerramos la sesión cURL
+		curl_close ($ch);	
+		// imprimir en formato JSON
+		header('Content-type: application/json'); 	
+		print_r($remote_server_output); 
+
+		
--- wsifin/ws_obtener_factura.php
+++ wsifin/ws_obtener_factura.php
@@ -68,8 +68,53 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                             );
                 }
             }else{
-               $resultado=array("estado"=>3, 
+                if($accion=="ObtenerFechaActual"){
+                $fechaActual=obtenerFechaActualBaseDatos();
+                if($fechaActual==""){
+                 $estado=5;
+                 $mensaje = "Ocurrio un error al generar la fecha";
+                 $resultado=array("estado"=>$estado, 
+                            "mensaje"=>$mensaje, 
+                            "fecha"=>$fechaActual,
+                            "totalComponentes"=>0);
+                }else{
+                  $estado=6;
+                  $resultado=array(
+                            "estado"=>$estado,
+                            "mensaje"=>"Fecha Obtenida Correctamente", 
+                            "datos"=>$fechaActual, 
+                            "totalComponentes"=>1     
+                            );
+                }
+              }else{
+                if($accion=="VerificacionPagosCurso"){
+                    if(isset($datos['IdCurso'])&&isset($datos['Ci'])){
+                      $facturasEncontradas=obtenerValidacionFacturaCurso($datos['IdCurso'],$datos['Ci']);
+                      if(count($facturasEncontradas)==0){
+                           $estado=7;
+                           $mensaje = "No existen registros";
+                           $resultado=array("estado"=>$estado, 
+                            "mensaje"=>$mensaje, 
+                            "Pagos"=>$facturasEncontradas,
+                            "totalComponentes"=>0);
+                        }else{
+                          $estado=8;
+                          $resultado=array(
+                            "estado"=>$estado,
+                            "mensaje"=>"Se encontraron los registros", 
+                            "pagos"=>$facturasEncontradas, 
+                            "totalComponentes"=>count($facturasEncontradas)    
+                            );
+                        }
+                    }else{
+                       $resultado=array("estado"=>9, 
+                            "mensaje"=>"Los parametros no son correctos"); 
+                    }
+                }else{
+                  $resultado=array("estado"=>3, 
                             "mensaje"=>"No existe la Accion Solicitada.");
+                } 
+              } 
             }        
          }
         }else{
@@ -84,7 +129,42 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     header('Content-type: application/json');
     echo json_encode($resp);
 }
-
+function obtenerValidacionFacturaCurso($curso,$ci){
+  $dbh = new Conexion();
+  $sqlX="SELECT sfd.codigo,sfd.cod_solicitudfacturacion,f.codigo as codFactura,f.nro_factura,cod_claservicio,
+CASE
+    WHEN sf.tipo_solicitud =1 THEN (SELECT IdServicio from simulaciones_servicios where codigo=sf.cod_simulacion_servicio)
+    WHEN sf.tipo_solicitud = 7  THEN sfd.cod_curso    
+    ELSE sf.cod_simulacion_servicio
+END as cod_padre,
+sf.tipo_solicitud
+from solicitudes_facturaciondetalle sfd
+join solicitudes_facturacion sf on sf.codigo=sfd.cod_solicitudfacturacion
+join facturas_venta f on f.cod_solicitudfacturacion=sf.codigo
+WHERE (sf.ci_estudiante='$ci' or sfd.ci_estudiante='$ci') and sf.tipo_solicitud in (2,6,7) 
+and f.cod_estadofactura<>2
+HAVING cod_padre=$curso;";
+  $stmtX = $dbh->prepare($sqlX);
+  $stmtX->execute();
+  $arrayFacturas=[];
+  $index=0;
+  while ($row = $stmtX->fetch(PDO::FETCH_ASSOC)) {
+    $arrayFacturas[$index]=array('Modulo' => $row['cod_claservicio'],'IdFactura' => $row['codFactura'],'CI' => $ci);
+    $index++;
+  }
+  return $arrayFacturas;
+}
+function obtenerFechaActualBaseDatos(){
+  $dbh = new Conexion();
+  $sqlX="SELECT NOW() as fecha";
+  $stmtX = $dbh->prepare($sqlX);
+  $stmtX->execute();
+  $fechaActual="";
+  while ($row = $stmtX->fetch(PDO::FETCH_ASSOC)) {
+    $fechaActual=$row['fecha'];
+  }
+  return $fechaActual;
+}
 function obtenerDatosFactura($codigo){
   require_once __DIR__.'/../conexion.php';
   $dbh = new Conexion();
