5f50012424e6a02a2c2aa4fec3ddcd7610d88243
Marco Luna <lunagonzalesmarco@gmail.com>
2021-02-25 01:05:54 -0400

    usuario externo

--- sp/test_servicios.php
+++ sp/test_servicios.php
@@ -2,31 +2,26 @@
 require_once '../conexion.php';
 require_once '../functions.php';
 
-  // $sKey = "c066ffc2a049cf11f9ee159496089a15";
-
-$direccion=obtenerValorConfiguracion(42);//direccion des servicio web
-    $sIde = "ifinanciero";
-    $sKey = "ce94a8dabdf0b112eafa27a5aa475751";  
-    // OBTENER MODULOS PAGADOS x CURSO
-    // $parametros=array("sIdentificador"=>$sIde, "sKey"=>$sKey, 
-    //         "accion"=>"ObtenerModulosPagados", 
-    //         "Identificacion"=>$ci_estudiante, //7666922 ci del alumno
-    //         "IdCurso"=>$IdCurso); //1565 
-
-    $parametros=array("sIdentificador"=>$sIde, "sKey"=>$sKey, 
-            "accion"=>"ObtenerModuloxPagarPagadoySaldo", 
-            "Identificacion"=>7666922, //7666922 ci del alumno
-            "IdCurso"=>1565); //1565
-    $parametros=json_encode($parametros);
+    $sIdentificador = "ifinanciero";
+    $sKey="ce94a8dabdf0b112eafa27a5aa475751";
+    $nombreuser="consultafinanzas@ibnorca.org";
+    $password="consulta2021";
+    $claveuser=$password;
+    $claveuser=md5($password);
+    $datos=array("sIdentificador"=>$sIdentificador, "sKey"=>$sKey, 
+                 "operacion"=>"Login", "nombreUser"=>$nombreuser, "claveUser"=>$claveuser);
+    $datos=json_encode($datos);
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,"http://ibnored.ibnorca.org/wsibnob/capacitacion/ws-inscribiralumno.php"); //PRUEBA
-    // curl_setopt($ch, CURLOPT_URL,$direccion."capacitacion/ws-inscribiralumno.php");
+    curl_setopt($ch, CURLOPT_URL,"http://ibnored.ibnorca.org/wsibno/verifica/ws-user-personal.php");
     curl_setopt($ch, CURLOPT_POST, TRUE);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $parametros);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $datos);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $remote_server_output = curl_exec ($ch);
     curl_close ($ch);
-    header('Content-type: application/json');   
+    $obj=json_decode($remote_server_output);
+
+    header('Content-type: application/json');     
     print_r($remote_server_output); 
+
 
 ?>
