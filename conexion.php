bc0e780c20ad71b91b63c4f5a5b5d0b7cf1b1403
Marco Luna <lunagonzalesmarco@gmail.com>
2021-11-14 22:37:42 -0400

    0

--- conexion.php
+++ conexion.php
@@ -19,8 +19,7 @@ class Conexion extends PDO {
 public function __construct() {
       //Sobreescribo el método constructor de la clase PDO.
       try{
-         parent::__construct($this->tipo_de_base.':host='.$this->host.';dbname='.$this->nombre_de_base.';port='.$this->port, $this->usuario, $this->contrasena,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));// 
-         //                                                                                                                                                                                               $this->nombre_de_base->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf8'");
+         parent::__construct($this->tipo_de_base.':host='.$this->host.';dbname='.$this->nombre_de_base.';port='.$this->port, $this->usuario, $this->contrasena,array(PDO::ATTR_PERSISTENT => 'buff',PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));//
       }catch(PDOException $e){
          echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
          exit;
