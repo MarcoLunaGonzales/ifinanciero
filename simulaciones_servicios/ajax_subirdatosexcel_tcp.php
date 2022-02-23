791274d5a9eeb2d5c59da74e4c61b3553b8055aa
= <=>
2022-02-23 18:33:02 -0400

    ajax subir excel tcp

--- simulaciones_servicios/ajax_subirdatosexcel_tcp.php
+++ simulaciones_servicios/ajax_subirdatosexcel_tcp.php
@@ -58,6 +58,9 @@ if(isset($_FILES['file_excel'])){
                     $depto_pais=trim($Row[7]);
                     $array_depto_pais=explode("-", $depto_pais);
                     $pais=$array_depto_pais[1];
+                    if($pais==''){
+                        $pais='Bolivia';
+                    }
                     $depto=$array_depto_pais[0];
                 }
             }
@@ -104,7 +107,8 @@ if(isset($_FILES['file_excel'])){
                 if(isset($Row[1])){$nombre=trim($Row[1]);};
                 if(isset($Row[4])){$marca=trim($Row[4]);};
                 if(isset($Row[5])){$norma=trim($Row[5]);};
-                if(isset($Row[7])){$sello=trim($Row[7]);};
+                // if(isset($Row[7])){$sello=trim($Row[7]);};
+                $sello=1;
                 if(isset($Row[8])){$direccion=trim($Row[8]);};
                 if(!empty($nombre) && !empty($marca)){
                     //echo $nombre."**".$marca."**".$norma."**".$sello."**".$direccion."<br><br>";
