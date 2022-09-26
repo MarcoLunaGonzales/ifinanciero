9563dbd01b7af36eda8dbdaeeae737a7f29ad75f
Marco Laptop <lunagonzalesmarco@gmail.com>
2022-09-26 13:41:30 -0400

    conexion

--- conexion.php
+++ conexion.php
@@ -1,19 +1,19 @@
 <?php 
 class Conexion extends PDO { 
 
-      // private $tipo_de_base = 'mysql';
-      // private $host = 'localhost';
-      // private $nombre_de_base = 'ibnorca12092022';
-      // private $usuario = 'root';
-      // private $contrasena = '4868422Marco';
-      // private $port = '3306'; 
-
       private $tipo_de_base = 'mysql';
-      private $host = 'lpsit.ibnorca.org';
-      private $nombre_de_base = 'bdifinanciero';
-      private $usuario = 'ingresofm';
-      private $contrasena = 'minka123';
-      private $port = '4606'; 
+      private $host = 'localhost';
+      private $nombre_de_base = 'ibnorca12092022';
+      private $usuario = 'root';
+      private $contrasena = '4868422Marco';
+      private $port = '3306'; 
+
+      // private $tipo_de_base = 'mysql';
+      // private $host = 'lpsit.ibnorca.org';
+      // private $nombre_de_base = 'bdifinanciero';
+      // private $usuario = 'ingresofm';
+      // private $contrasena = 'minka123';
+      // private $port = '4606'; 
 
      // private $tipo_de_base = 'mysql';
      //  private $host = '192.168.30.35';
