224813cc3c926157b3cbad4c9343a5e15122cdb3
Marco Laptop <lunagonzalesmarco@gmail.com>
2022-11-30 11:59:24 -0400

    cambios

--- conexion_externa.php
+++ conexion_externa.php
@@ -9,20 +9,20 @@ class ConexionIBNORCA extends PDO {
       // private $port = '3306'; 
 
       //  //CONEXION IBNORCA PRUEBAS
-      private $tipo_de_base = 'mysql';
-      private $host = 'lpsit.ibnorca.org';
-      private $nombre_de_base = 'ibnorca';
-      private $usuario = 'ingresobd';
-      private $contrasena = 'ingresoibno';
-      private $port = '3360'; 
+      // private $tipo_de_base = 'mysql';
+      // private $host = 'lpsit.ibnorca.org';
+      // private $nombre_de_base = 'ibnorca';
+      // private $usuario = 'ingresobd';
+      // private $contrasena = 'ingresoibno';
+      // private $port = '3360'; 
 
      //CONEXION INTERNA PRUEBAS 
-     // private $tipo_de_base = 'mysql';
-     //  private $host = '192.168.30.35';
-     //  private $nombre_de_base = 'ibnorca';
-     //  private $usuario = 'ibnofinanciero';
-     //  private $contrasena = 'Financiero1bn0';
-     //  private $port = '3306'; 
+     private $tipo_de_base = 'mysql';
+      private $host = '192.168.30.35';
+      private $nombre_de_base = 'ibnorca';
+      private $usuario = 'ibnofinanciero';
+      private $contrasena = 'Financiero1bn0';
+      private $port = '3306'; 
 
         
   public function __construct() {
