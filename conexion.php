85f9e236877d5204c2322609cd0c7a768957c5d4
isullcamani <sullcamani@gmail.com>
2023-01-31 22:07:17 -0400

    commit formato boletas

--- conexion.php
+++ conexion.php
@@ -17,11 +17,18 @@ class Conexion extends PDO {
       // private $port = '3306'; 
 
 
-      private $tipo_de_base = 'mysql';
+      // private $tipo_de_base = 'mysql';
+      // private $host = 'localhost';
+      // private $nombre_de_base = 'financiero_cierre2022_oficial';
+      // private $usuario = 'root';
+      // private $contrasena = '4868422Marco';
+      // private $port = '3306'; 
+
+     private $tipo_de_base = 'mysql';
       private $host = 'localhost';
-      private $nombre_de_base = 'financiero_cierre2022_oficial';
+      private $nombre_de_base = 'ibnofinanciero5000';
       private $usuario = 'root';
-      private $contrasena = '4868422Marco';
+      private $contrasena = '12345678';
       private $port = '3306'; 
 
 public function __construct() {
