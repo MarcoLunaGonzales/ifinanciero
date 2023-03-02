81eb2e801551926e74d6f29bd9b66e7a75963d27
Marco Laptop <lunagonzalesmarco@gmail.com>
2023-03-02 12:03:26 -0400

    cambios

--- conexion.php
+++ conexion.php
@@ -19,7 +19,7 @@ class Conexion extends PDO {
 
       // private $tipo_de_base = 'mysql';
       // private $host = 'localhost';
-      // private $nombre_de_base = 'financiero_2022';
+      // private $nombre_de_base = 'financiero0223';
       // private $usuario = 'root';
       // private $contrasena = '4868422Marco';
       // private $port = '3306'; 
