a44a31e770e75c6cf2162ec4c04520858ae64a17
roalxDcar <roalmollericona@gmail.com>
2023-03-08 13:01:12 -0400

    .

--- conexion.php
+++ conexion.php
@@ -10,12 +10,20 @@ class Conexion extends PDO {
       // private $port = '4606'; 
 
 
+      // IBNORCA PRUEBAS OFICIAL EXTERNO
       // private $tipo_de_base = 'mysql';
-      // private $host = 'localhost';
-      // private $nombre_de_base = 'financiero0223';
-      // private $usuario = 'root';
-      // private $contrasena = '4868422Marco';
-      // private $port = '3306'; 
+      // private $host = 'lpsit.ibnorca.org';
+      // private $nombre_de_base = 'bdifinanciero';
+      // private $usuario = 'ingresobd';
+      // private $contrasena = 'ingresoibno';
+      // private $port = '3360'; 
+
+      private $tipo_de_base = 'mysql';
+      private $host = 'localhost';
+      private $nombre_de_base = 'ifinanciero';
+      private $usuario = 'root';
+      private $contrasena = '';
+      private $port = '3306'; 
 
 
       //CONEXION IBNORCA PRUEBAS
--- conexion_externa.php
+++ conexion_externa.php
@@ -17,12 +17,20 @@ class ConexionIBNORCA extends PDO {
       // private $port = '3360'; 
 
      //CONEXION INTERNA PRUEBAS 
-     private $tipo_de_base = 'mysql';
-      private $host = '192.168.30.35';
-      private $nombre_de_base = 'ibnorca';
-      private $usuario = 'ibnofinanciero';
-      private $contrasena = 'Financiero1bn0';
-      private $port = '3306'; 
+   //   private $tipo_de_base = 'mysql';
+   //    private $host = '192.168.30.35';
+   //    private $nombre_de_base = 'ibnorca';
+   //    private $usuario = 'ibnofinanciero';
+   //    private $contrasena = 'Financiero1bn0';
+   //    private $port = '3306'; 
+
+   // IBNORCA CONEXIÖN EXTERNA PRUEBAS
+   private $tipo_de_base = 'mysql';
+   private $host = 'lpsit.ibnorca.org';
+   private $nombre_de_base = 'ibnorca';
+   private $usuario = 'ingresobd';
+   private $contrasena = 'ingresoibno';
+   private $port = '3360'; 
 
      //CONEXION INTERNA PRUEBAS 
       // private $tipo_de_base = 'mysql';
