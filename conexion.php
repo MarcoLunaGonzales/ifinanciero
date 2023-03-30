2c379d23f7a6d11e74d39637e26514224d61f4b2
roalxDcar <roalmollericona@gmail.com>
2023-03-30 18:48:14 -0400

    verificacion de error 30-03-2023

--- conexion.php
+++ conexion.php
@@ -12,9 +12,9 @@ class Conexion extends PDO {
       // // LOCAL
       private $tipo_de_base = 'mysql';
       private $host = 'localhost';
-      private $nombre_de_base = 'financiero09032023';
+      private $nombre_de_base = 'ifinanciero';
       private $usuario = 'root';
-      private $contrasena = '4868422Marco';
+      private $contrasena = '';
       private $port = '3306'; 
 
       // PRUEBAS EXTERNO
