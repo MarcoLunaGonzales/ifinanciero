71acb714b4e9723a129c12b3fa6649cb6a6228c9
Ronald <45970198+roalxDcar@users.noreply.github.com>
2023-11-01 12:49:56 -0400

    .

--- personal/personalForm.php
+++ personal/personalForm.php
@@ -485,11 +485,13 @@ $statementestados_personal = $dbh->query($queryestados_personal);
                                 <label class="col-sm-2 col-form-label">AFP</label>
                                 <div class="col-sm-4">
                                     <div class="form-group">
-                                        <select name="cod_tipoafp" id="cod_tipoafp"  class="selectpicker form-control form-control-sm " data-style="btn btn-info" required disabled>
+                                        <select name="cod_tipoafp_view" id="cod_tipoafp_view"  class="selectpicker form-control form-control-sm " data-style="btn btn-info" required disabled>
                                             <?php while ($row = $statementtipos_afp->fetch()) { ?>
                                                 <option <?php /*if($cod_tipoafp == $row["codigo"]) echo "selected";*/ if($row["codigo"] == 3) echo "selected";?> value="<?=$row["codigo"];?>"><?=$row["nombre"];?></option>
                                             <?php } ?>
                                         </select>
+                                        <!-- GESTORA -->
+                                        <input type="hidden" name="cod_tipoafp" value="3">
                                     </div>
                                 </div>
 
