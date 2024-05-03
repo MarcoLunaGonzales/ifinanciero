bdf704995f97ee5310c1b219528f47f74bb0d6fa
Ronald <45970198+roalxDcar@users.noreply.github.com>
2024-05-03 13:13:35 -0400

    .

--- wsifin/ws_generate_invoice.php
+++ wsifin/ws_generate_invoice.php
@@ -210,7 +210,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                      * * Verificación de estado de Curso
                                      */
                                     $verficaCursoString = verificaVentaNoFacturada($sucursalId,$pasarelaId,$fechaFactura,$nitciCliente,$razonSocial,$importeTotal_x,$items,$CodLibretaDetalle,$tipoPago,$normas,$nroTarjeta,$idIdentificacion,$complementoCiCliente,$CorreoCliente,$idCliente,$usuario);
-                                    // echo $verficaCursoString ? 'Facturar' : 'Factura pendiente'; // false: No permite Facturar, true: Factura
+                                    // echo $verficaCursoString ? 'Facturar' : 'Factura pendiente'; //  true: Factura, false: No permite Facturar
                                     // exit;
                                     break;
                                 }
