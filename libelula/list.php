996e350ee4cadc9dfbff038979fad773fad72f44
Ronald <45970198+roalxDcar@users.noreply.github.com>
2024-06-28 12:01:25 -0400

    .

--- libelula/list.php
+++ libelula/list.php
@@ -13,9 +13,9 @@ ini_set('display_errors', 1);
     /*************************
      * ? LISTA DE REPORTES
      *************************/
-    $url_ecommerce          = obtenerValorConfiguracion(109);
-    $fecha_inicio = date('Y-m-01');
-    $fecha_fin    = date('Y-m-t');
+    $url_ecommerce = obtenerValorConfiguracion(109);
+    $fecha_inicio  = !empty($_GET['fecha_inicio']) ? $_GET['fecha_inicio'] : date('Y-m-01');
+    $fecha_fin     = !empty($_GET['fecha_fin']) ? $_GET['fecha_fin'] : date('Y-m-t');
     $url_ws                 = $url_ecommerce.'tienda/reporteLibelula.php';
     $sw_token               = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIzMDAxMiIsImlkVXN1YXJpbyI6IjMwMDEyIiwiY29ycmVvIjoid2VibWFzdGVyQGNvZS1lamVyY2l0by5jb20uYm8iLCJhdWQiOiJpYm5vcmNhIiwiaWF0IjoxMzU2OTk5NTI0LCJuYmYiOjEzNTcwMDAwMDB9.9VR9tWgtfSi_s9ix8qkZSl1fPYzCExJKMOPii_quXEE";
     $parametros = array(
@@ -95,19 +95,45 @@ ini_set('display_errors', 1);
                         </div>
                         <h4 class="card-title"><b>Cursos</b></h4>                    
                     </div>
+                    <div class="card-header">
+                        <div class="row justify-content-end">
+                            <div class="col-md-6">
+                                <form id="filter" action="index.php" method="GET">
+                                    <div class="row">
+                                        <input type="hidden" name="opcion" value="reporteLibelula">
+                                        <div class="col-md-5">                                    
+                                            <div class="form-group">
+                                                <label for="start-date">Fecha de Inicio:</label>
+                                                <input type="date" class="form-control" id="fecha_inicio" name="fecha_inicio" value="<?=$fecha_inicio?>">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-5">
+                                            <div class="form-group">
+                                                <label for="end-date">Fecha de Fin:</label>
+                                                <input type="date" class="form-control" id="fecha_fin" name="fecha_fin" value="<?=$fecha_fin?>">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-2 d-flex align-items-end">
+                                            <button type="submit" class="btn btn-success btn-sm">Filtrar</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                     <div class="card-body" id="data_solicitudes_facturacion_activas">
                         <table class="table" id="tablePaginator2">
                             <thead>
                                 <tr>  
-                                    <th width="30%"><small>#</small></th>
-                                    <th width="30%"><small>CursoId</small></th>
-                                    <th width="30%"><small>Total Pagado</small></th>
-                                    <th width="40%"><small>Libélula Respuesta</small></th>
+                                    <th width="10%"><small>Fecha</small></th>
+                                    <th width="10%"><small>CursoId</small></th>
+                                    <th width="10%"><small>Total Pagado</small></th>
+                                    <th width="10%"><small>Libélula Respuesta</small></th>
                                     
-                                    <th width="40%"><small>Estudiante</small></th>
-                                    <th width="40%"><small>Fecha Factura</small></th>
-                                    <th width="40%"><small>Estado</small></th>
-                                    <th width="40%"><small>CodFacturaVenta</small></th>
+                                    <th width="10%"><small>Estudiante</small></th>
+                                    <th width="10%"><small>Fecha Factura</small></th>
+                                    <th width="10%"><small>Estado</small></th>
+                                    <th width="10%"><small>CodFacturaVenta</small></th>
                                 </tr>
                             </thead>
                             <tbody>
